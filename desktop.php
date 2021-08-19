b9dc6de3a508367a78104e22f2340637cf60c359
Ornelstel <87016099+Ornelstel@users.noreply.github.com>
2021-08-19 11:59:17 +0200

    Premier commit

---
+++ desktop.php
@@ -0,0 +1,97 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/desktop.css">
+    <title>Document</title>
+</head>
+<body>
+    <header>
+        <img id="img1" src="assets/images/GBAF.jpg" alt="logo de GBAF">
+    </header> <hr>
+
+    <main>
+        <span>PRESENTATION DES ACTEURS</span>
+        <section>
+            <p>Le Groupement Banque Assurance Français (GBAF) est une fédération
+                représentant les 6 grands groupes français :<br><br>
+                ● BNP Paribas ;<br>
+                ● BPCE ;<br>
+                ● Crédit Agricole ;<br>
+                ● Crédit Mutuel-CIC ;<br>
+                ● Société Générale ;<br>
+                ● La Banque Postale.<br><br>
+                Même s’il existe une forte concurrence entre ces entités, elles vont toutes travailler
+                de la même façon pour gérer près de 80 millions de comptes sur le territoire
+                national.
+                Le GBAF est le représentant de la profession bancaire et des assureurs sur tous
+                les axes de la réglementation financière française. Sa mission est de promouvoir
+                l'activité bancaire à l’échelle nationale. C’est aussi un interlocuteur privilégié des pouvoirs publics. </p>
+        </section><hr>
+
+        <article>
+	         <span>LES ACTEURS ET PARTENAIRES</span>
+	    <section class="section1">
+                <div>
+                    <img id="img2" src="assets/images/formation_co.png" alt="logo de l´entreprise formation_co">
+                </div>
+			       <p>Formation&co est une association française présente sur tout le territoire.
+                    Nous proposons à des personnes issues de tout milieu de devenir entrepreneur grâce à un crédit et un accompagnement professionnel et personnalisé.
+                    Notre proposition : <br>
+                    ●	un financement jusqu’à 30 000€ ;<br>
+                    ●	un suivi personnalisé et gratuit ;<br>
+                    ●	une lutte acharnée contre les freins sociétaux et les stéréotypes.<br>
+
+                    Le financement est possible, peu importe le métier : coiffeur, banquier, éleveur de chèvres… . Nous collaborons avec des personnes talentueuses et motivées.
+                    Vous n’avez pas de diplômes ? Ce n’est pas un problème pour nous ! Nos financements s’adressent à tous.</p>
+			   </div>
+
+		</section>
+
+		<section class="section1">
+			   <div>
+                    <img id="img3" src="assets/images/protectpeople.png" alt="logo de l´entreprise protectpeople">
+			   </div>
+               <p>Protectpeople finance la solidarité nationale.
+                        Nous appliquons le principe édifié par la Sécurité sociale française en 1945 : permettre à chacun de bénéficier d’une protection sociale.
+
+                        Chez Protectpeople, chacun cotise selon ses moyens et reçoit selon ses besoins.
+                        Proectecpeople est ouvert à tous, sans considération d’âge ou d’état de santé.
+                        Nous garantissons un accès aux soins et une retraite.
+                        Chaque année, nous collectons et répartissons 300 milliards d’euros.
+                        Notre mission est double :<br>
+                        ●	sociale : nous garantissons la fiabilité des données sociales ;<br>
+                        ●	économique : nous apportons une contribution aux activités économiques.</p><br>
+		</section>
+
+        <section class="section1">
+			   <div>
+                    <img id="img4" src="assets/images/CDE.png" alt="logo de l´entreprise CDE">
+			   </div>
+               <p>La CDE (Chambre Des Entrepreneurs) accompagne les entreprises dans leurs démarches de formation. 
+                    Son président est élu pour 3 ans par ses pairs, chefs d’entreprises et présidents des CDE.</p>
+		</section>
+
+        <section class="section1">
+			   <div>
+                       <img id="img5" src="assets/images/Dsa_france.png" alt="logo de l´entreprise Dsa_france">
+			   </div>
+               <p>Dsa France accélère la croissance du territoire et s’engage avec les collectivités territoriales.
+                        Nous accompagnons les entreprises dans les étapes clés de leur évolution.
+                        Notre philosophie : s’adapter à chaque entreprise.
+                        Nous les accompagnons pour voir plus grand et plus loin et proposons des solutions de financement adaptées à l’échelle nationale.
+                        C’est aussi un interlocuteur privilégié des pouvoirs publics.</p>
+		</section>
+	   </article>
+
+    </main><hr>
+    <footer id="footer1">
+
+            <span> | Mentions légales | Contact | </span>
+
+    </footer>
+
+</body>
+</html>
